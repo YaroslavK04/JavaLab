ecfa561040b44a77abdd2a58847c0b11b99269db
YaroslavK04 <114668684+YaroslavK04@users.noreply.github.com>
2025-03-12 09:21:07 +0300

    lab4dop

--- Lab4Java/src/RecIntegral.java
+++ Lab4Java/src/RecIntegral.java
@@ -1,28 +1,35 @@
 
-import java.io.Serializable;
+import java.io.Externalizable ;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author yaros
- */
-public class RecIntegral implements Serializable
-    {
-        private static final long serialVersionUID = 1L;
-        private String DowbLimit, UpLimit,Step,Result;
-        public RecIntegral(String DowbLimit,String UpLimit,String Step,String Result) {
-            this.DowbLimit = DowbLimit;
-            this.UpLimit = UpLimit;
-            this.Step = Step;
-            this.Result = Result;
-        }
-        
-        public String[] Ret(){
-            return new String[]{DowbLimit, UpLimit,Step,Result};
-        }
-        
+public class RecIntegral implements Externalizable 
+{
+    private static final long serialVersionUID = 1L;
+    private String DowbLimit, UpLimit,Step,Result;
+    public RecIntegral() {}
+    public RecIntegral(String DowbLimit,String UpLimit,String Step,String Result) {
+        this.DowbLimit = DowbLimit;
+        this.UpLimit = UpLimit;
+        this.Step = Step;
+        this.Result = Result;
     }
+
+    public String[] Ret(){
+        return new String[]{DowbLimit, UpLimit,Step,Result};
+    }
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(DowbLimit);
+        out.writeObject(UpLimit);
+        out.writeObject(Step);
+        out.writeObject(Result);
+    }
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        DowbLimit = (String) in.readObject();
+        UpLimit = (String) in.readObject();
+        Step = (String) in.readObject();
+        Result = (String) in.readObject();
+    }
+}
